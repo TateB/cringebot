6ca17facd2669d6dfb2e26013caff6b01573bb9c
TateB <me@tatebulic.com.au>
2020-07-24 13:54:54 +1000

    level command works with mentions of other users

--- commands/level.js
+++ commands/level.js
@@ -4,10 +4,18 @@ module.exports = {
 	name: 'level',
 	description: "Gets the user's level",
 	execute(message, args, db) {
-		let dbUser = db.get("users").get(message.author)
+		var specifiedUser = message.author
+
+		// if there is a mentioned user 
+		if (message.mentions.users.first() != undefined) {
+			specifiedUser = message.mentions.users.first()
+		}
+
+
+		let dbUser = db.get("users").get(specifiedUser)
 		const embed = new Discord.MessageEmbed()
-		.setTitle(`${message.author.username}'s Stats`)
-		.setThumbnail(message.author.displayAvatarURL())
+		.setTitle(`${specifiedUser.username}'s Stats`)
+		.setThumbnail(specifiedUser.displayAvatarURL())
 		.addFields(
 			{ name: "Level", value: `${dbUser.get("level").value()}` },
 			{ name: "XP", value: `${dbUser.get("xp").value()} **/** ${dbUser.get("nextLevelXp").value()}` }
