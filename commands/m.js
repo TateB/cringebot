6b8fdae359ccdf241b712cf88c5f4cac29f8e0d4
TateB <me@tatebulic.com.au>
2020-07-29 16:21:45 +1000

    added full music command back

--- commands/m.js
+++ commands/m.js
@@ -1,9 +1,9 @@
 const music = require('../utilities/music.js')
 
 module.exports = {
-	name: 'music',
-	description: 'music comnmand for music playing in voice channels',
-	usage: '!music play [name/link]',
+	name: 'm',
+	description: 'music comnmand for music playing in voice channels (shortened)',
+	usage: '!m play [name/link]',
 	execute(message, args, db, distube) {
 		music.distubeCommandHandler(message, args, distube)
 	}
