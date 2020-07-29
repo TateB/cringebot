23f1eeb2f50a04e70b9a216290271f0a17a83407
TateB <me@tatebulic.com.au>
2020-07-29 15:41:43 +1000

    changed "!music" to "!m"

--- commands/music.js
+++ commands/music.js
@@ -1,7 +1,7 @@
 const music = require('./../utilities/music.js')
 
 module.exports = {
-	name: 'music',
+	name: 'm',
 	description: 'music command for music!',
 	execute(message, args, db, distube) {
 		music.distubeCommandHandler(message, args, distube)
