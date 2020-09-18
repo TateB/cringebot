6ffdd2f16cabcb97832c2a5228ddb6def39a47fb
TateB <me@tatebulic.com.au>
2020-09-18 08:52:34 +0000

    added extra bonus to pickle command

--- commands/pickle.js
+++ commands/pickle.js
@@ -7,8 +7,10 @@ module.exports = {
     usage: "!pickle",
     execute(message, args){
 	if(message.author.id == "179447175812612096"){
-	    message.channel.send("Holy shit <@179447175812612096>, I don't have a long enough ruler for you");
-	}else {
+	    	message.channel.send("Holy shit <@179447175812612096>, I don't have a small enough ruler for you");
+	} else if (message.author.id == "132399256525864961") {
+		message.channel.send("Wow! <@132399256525864961>, You're off the charts big!")
+	} else {
 	    var shasum = crypto.createHash('sha1');
 	    shasum.update(message.author.id);
 	    var hash = shasum.digest('hex');
