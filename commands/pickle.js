448aaf53cc053fd1d500997256492f5a979cd016
Sam Bonnekamp <samuel@bonnekamp.net>
2020-09-20 11:04:19 +0800

    fixed issue 9 where pickle command wouldnt parse users IDs correctly

--- commands/pickle.js
+++ commands/pickle.js
@@ -7,14 +7,16 @@ module.exports = {
     usage: "!pickle",
     execute(message, args){
 	if(message.author.id == "179447175812612096"){
-	    	message.channel.send("Holy shit <@179447175812612096>, I don't have a small enough ruler for you");
+	    	message.channel.send("Holy shit <@179447175812612096>, I don't have a large enough ruler for you");
 	} else if (message.author.id == "132399256525864961") {
 		message.channel.send("Wow! <@132399256525864961>, You're off the charts big!")
 	} else {
-	    var shasum = crypto.createHash('sha1');
+	    var shasum;
+	    var hash;
+	    shasum = crypto.createHash('sha1');
 	    shasum.update(message.author.id);
-	    var hash = shasum.digest('hex');
-	    message.channel.send("Woah <@" + message.author.id + ">, your dick is " + (parseInt(hash)%30) + "cm long. Nice cock!");   
+	    hash = shasum.digest('hex');
+	    message.channel.send("Woah <@" + message.author.id + ">, your dick is " + (parseInt(hash, 16)%30) + "cm long. Nice cock!");   
 	}	   
     }
 }
